741cc868907575a964b6b49c425efd3765d9fc13
Julia <juliaduch33@gmail.com>
2024-12-13 10:49:45 +0100

    add function for graph visualization

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import heapq
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 def dijkstra(graph, start, end):
@@ -64,3 +66,49 @@ def check_node_connections(graph, node):
         print(f"Połączenia dla wierzchołka {node}:")
         for neighbor, weight in connections:
             print(f" - {neighbor} (waga: {weight})")
+
+
+def visualize_graph(start_node, static_graph, waypoints):
+    if start_node in static_graph and all(wp in static_graph for wp in waypoints):
+        try:
+            path, cost = round_trip_path(static_graph, start_node, waypoints)
+            print(
+                f"Najkrótsza trasa z {start_node}, przez {waypoints}, z powrotem do {start_node}: {path} o koszcie {cost}")
+
+            G = nx.Graph()
+
+            for node, edges in static_graph.items():
+                for neighbor, weight in edges:
+                    G.add_edge(node, neighbor, weight=weight)
+
+            pos = nx.spring_layout(G)
+
+            node_colors = []
+            for node in G.nodes:
+                if node == start_node:
+                    node_colors.append('#98FB98')
+                elif node in waypoints:
+                    node_colors.append('#FFD700')
+                elif node in path:
+                    node_colors.append('#ADD8E6')
+                else:
+                    node_colors.append('#D3D3D3')
+
+            plt.figure(figsize=(12, 8))
+            nx.draw(G, pos, with_labels=True, node_color=node_colors,
+                    node_size=600, font_size=10)
+            labels = nx.get_edge_attributes(G, 'weight')
+            nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
+
+            path_edges = list(zip(path, path[1:]))
+            nx.draw_networkx_edges(
+                G, pos, edgelist=G.edges, edge_color='black')
+            nx.draw_networkx_edges(
+                G, pos, edgelist=path_edges, edge_color='red', width=2)
+
+            plt.title("Graf z zaznaczoną trasą powrotną")
+            plt.show()
+        except ValueError as e:
+            print(e)
+        else:
+            print("Podano nieprawidłowe wierzchołki.")
